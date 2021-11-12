3056ab8febf3061e32b8cca711d34dd5e4ba0402
Tony Berglund <tony_berglund@outlook.com>
2021-11-12 14:12:03 +0100

    IndustriellMaskinparkAPI

--- IndustriellMaskinParkApi/MaskinParkApi.cs
+++ IndustriellMaskinParkApi/MaskinParkApi.cs
@@ -11,6 +11,7 @@ using Newtonsoft.Json;
 using Microsoft.Azure.Cosmos.Table.Queryable;
 using IndustriellMaskinParkApi.Models;
 using System.Linq;
+using System.Collections.Generic;
 using Microsoft.AspNetCore.Cors;
 
 namespace IndustriellMaskinParkApi
@@ -41,22 +42,46 @@ namespace IndustriellMaskinParkApi
         [EnableCors]
         [FunctionName("Put")]
         public static async Task<IActionResult> Put(
-            [HttpTrigger(AuthorizationLevel.Anonymous, "put", Route = "maskinpark/{MachineId}")] HttpRequest req)
+            [HttpTrigger(AuthorizationLevel.Anonymous, "put", Route = "maskinpark/{MachineId}")] HttpRequest req,
+            [Table("maskinpark", Connection = "AzureWebJobsStorage")] CloudTable machineTable,
+            string machineid,
+            ILogger log)
+            {
+            log.LogInformation("Update data by send command.");
+            var query = new TableQuery<DataTableEntity>();
+            var result = await machineTable.ExecuteQuerySegmentedAsync(query, null);
+            var machine = result.Select(Mapper.ToMachine).Where(m => m.MachineId == machineid).FirstOrDefault();
+      /*      var response = new IndustrialMachinepark { MachineName = machine.MachineName, Status = machine.Status  }*/;
+      
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            var updateData = JsonConvert.DeserializeObject<IndustrialMachinepark>(requestBody);
+            updateData.MachineName = machine.MachineName;
+            updateData.Status = machine.Status;
+
+            if (updateData is null || updateData.MachineId != machineid) return new BadRequestResult();
+
+            var dataTable = updateData.ToTableEntity();
+            dataTable.ETag = "*";
+            var operation = TableOperation.Replace(dataTable);
+            await machineTable.ExecuteAsync(operation);
+
+            return new NoContentResult();
+        }
 
         [EnableCors]
         [FunctionName("Get")]
         public static async Task<IActionResult> Get(
-        [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "Maskinpark")] HttpRequest req,
-        [Table("maskinpark", Connection = "AzureWebJobsStorage")] CloudTable machineTable,
-        ILogger log)
-        {
-            log.LogInformation("Get all machines");
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "Maskinpark")] HttpRequest req,
+            [Table("maskinpark", Connection = "AzureWebJobsStorage")] CloudTable machineTable,
+            ILogger log)
+            {
+                log.LogInformation("Get all machines");
 
-            var query = new TableQuery<DataTableEntity>();
-            var result = await machineTable.ExecuteQuerySegmentedAsync(query, null);
-            var response = new IndustrialMachines { Machines = result.Select(Mapper.ToMachine).ToList() };
+                var query = new TableQuery<DataTableEntity>();
+                var result = await machineTable.ExecuteQuerySegmentedAsync(query, null);
+                var response = new IndustrialMachines { Machines = result.Select(Mapper.ToMachine).ToList() };
 
-            return new OkObjectResult(response);
-        }
+                return new OkObjectResult(response);
+            }
     }
 }
